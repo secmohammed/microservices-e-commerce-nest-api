1c93469475382af42f5e37dfff361d9d01044768
Mohammed Osama <mohammedosama@ieee.org>
2019-09-03 12:27:49 +0200

    WIP

--- server/services/payments/src/main.ts
+++ server/services/payments/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { Transport } from "@nestjs/microservices";
 import { config } from "@commerce/shared";
+// import { stripe } from "./utils/stripe";
 async function bootstrap() {
     const app = await NestFactory.createMicroservice(AppModule, {
         transport: Transport.REDIS,
@@ -10,5 +11,31 @@ async function bootstrap() {
         }
     });
     await app.listen(() => console.log(`payments module is listening `));
+    // user inserts his payment card at the frontend form ( pass the other data of this user as a second parameter such as address, name etc..).
+    // stripe api sends us a token to our website.
+    // we deliver this token alongside with the rest of the response to our server.
+    // we store that to the payment methods table alongside the token we got from stripe.
+    // create a customer using the stripe token, and pass data like email,name etc.. we collected earlier on register.
+    //  we can attach the payment_method_id for more info.
+    // we can charge the user for whatever he bought in cents using the stripe token/ payment_method_id and optionally the customer associated to this charge.
+
+    // stripe.customers
+    //     .create({
+    //         email: "foo-customer@example.com"
+    //     })
+    //     .then(customer => {
+    //         console.log(customer);
+    //         return stripe.customers.createSource(customer.id, {
+    //             source: "tok_visa"
+    //         });
+    //     })
+    //     .then(source => {
+    //         console.log(source.customer);
+    //         return stripe.charges.create({
+    //             amount: 1600,
+    //             currency: "usd",
+    //             customer: source.customer as string
+    //         });
+    //     });
 }
 bootstrap();
---
+++ server/services/payments/src/utils/stripe.ts
@@ -0,0 +1,2 @@
+import * as Stripe from "stripe";
+export const stripe = new Stripe("sk_test_J29GVsb1mCVACXR6LIz1wrW7");
